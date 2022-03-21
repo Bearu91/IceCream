432978e1ffad3797068a36b0ad889135247db478
Bearu91 <birek1991@gmail.com>
2022-03-21 14:33:16 +0100

    IceCreamSite

---
+++ src/modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(button => button.addEventListener('click', () => toggleModal()));
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
